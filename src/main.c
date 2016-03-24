a1fdc39385d12b0e2d8314d4ecbbe59c2c916946
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-03-24 19:41:44 -0300

    Adding the main file template

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  wait_port.c
+ *
+ *    Description:  Main file of the project
+ *
+ *        Created:  03/24/2016 19:40:56
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+#include <stdlib.h>
+
+
+int
+main (int argc, char* argv[])
+{
+
+    return EXIT_SUCCESS;
+}
