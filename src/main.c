3291ccfe687fa7144c5f83affc929a4b1af5b4db
{NAME} <gustavopantuza@gmail.com>
2016-03-25 00:42:36 -0300

    Using the options parsing and colors at main function

--- src/main.c
+++ src/main.c
@@ -16,10 +16,32 @@
 
 #include <stdlib.h>
 
+#include "args.h"
+#include "colors.h"
+
+
 
 int
 main (int argc, char* argv[])
 {
 
+    /* Read command line options */
+    options_t options;
+    options_parser(argc, argv, &options);
+
+
+#ifdef DEBUG
+    fprintf(stdout, BLUE "Command line options:\n" NO_COLOR);
+    fprintf(stdout, BROWN "help: %d\n" NO_COLOR, options.help);
+    fprintf(stdout, BROWN "version: %d\n" NO_COLOR, options.version);
+    fprintf(stdout, BROWN "use colors: %d\n" NO_COLOR, options.use_colors);
+    fprintf(stdout, BROWN "filename: %s\n" NO_COLOR, options.file_name);
+#endif
+
+
+    /* Do your magic here :) */
+
+
     return EXIT_SUCCESS;
 }
+
