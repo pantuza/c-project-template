be1241c0a0588763b759e57a1445a8f0f6d6448c
{NAME} <gustavopantuza@gmail.com>
2016-03-25 00:41:42 -0300

    Created the messages header file

---
+++ src/messages.h
@@ -0,0 +1,37 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  messages.h
+ *
+ *    Description:  Header file of program messages
+ *
+ *        Created:  24/03/2015 22:30:42
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+
+
+#define __PROGRAM_NAME__ "Program Name"
+#define __PROGRAM_VERSION__ "0.0.1"
+#define __PROGRAM_AUTHOR__ "Gustavo Pantuza (gustavopantuza@gmail.com)"
+
+
+void help ();
+void usage ();
+void version ();
+void description ();
+void options ();
+void author ();
+
+
+#endif // MESSAGES_H
