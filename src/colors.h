d570d36e2e26a709d7a397854d167bf160038b65
{NAME} <gustavopantuza@gmail.com>
2016-03-25 00:41:07 -0300

    Created the header file that defines the colors for pretty printing

---
+++ src/colors.h
@@ -0,0 +1,38 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  colors.h
+ *
+ *    Description:  This file define the output colors used by the program
+ *
+ *        Created:  24/03/2015 22:20:40 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+
+#ifndef COLORS_H
+#define COLORS_H
+
+
+
+/**
+ *
+ * ANSI color definitions for pretty-print
+ *
+ */
+
+#define NO_COLOR    "\x1b[0m"
+#define RED         "\x1b[31m"
+#define BLUE        "\x1b[34m"
+#define GREEN       "\x1b[32m"
+#define BROWN       "\x1b[33m"
+#define GRAY        "\x1b[37m"
+
+
+#endif // COLORS_H
