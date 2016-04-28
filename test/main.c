49ce822ee36ecb3eec62735e60b233865e19df4e
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-04-28 19:55:39 -0300

    Created the test runner file

---
+++ test/main.c
@@ -0,0 +1,51 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Main execution of the tests using cmocka
+ *
+ *        Created:  04/28/2016 19:21:37
+ *       Compiler:  gcc
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Software Community
+ *
+ * ============================================================================
+ */
+
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+
+/* include here your files that contain test functions */
+
+
+
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+
+
+/*
+ * Test runner function
+ */
+int
+main(void) {
+
+    /**
+     * Insert here your test functions
+     */
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+    };
+
+
+    /* Run the tests */
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
