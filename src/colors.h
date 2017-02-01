76d40f22c00bf1be90dacfeb8fbbaa18cbe99196
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-01 21:30:36 -0200

    Adding new color definitions

--- src/colors.h
+++ src/colors.h
@@ -29,9 +29,11 @@
 
 #define NO_COLOR    "\x1b[0m"
 #define RED         "\x1b[31m"
-#define BLUE        "\x1b[34m"
 #define GREEN       "\x1b[32m"
 #define BROWN       "\x1b[33m"
+#define BLUE        "\x1b[34m"
+#define MAGENTA     "\x1b[35m"
+#define CYAN        "\x1b[36m"
 #define GRAY        "\x1b[37m"
 
 
